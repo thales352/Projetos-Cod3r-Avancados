5dcff6cdf13005cf82a0b41158dbaba8729524e9
thales352 <thalesiftm@gmail.com>
2022-02-14 13:44:04 -0300

    main

--- monty-hall-logic/functions/CriarPortas.ts
+++ monty-hall-logic/functions/CriarPortas.ts
@@ -1,22 +1,22 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { PortaModel } from "../model/porta";
+import { PortaModel } from '../model/porta';
 
-export default (qtde:number,comPresente:number):PortaModel[] => {
-  return Array.from({length:qtde},(_,i)=>{
-      const num = i+1
-      const temPresente =num === comPresente
-      return new PortaModel(num,temPresente)
-  })
+export default (qtde: number, comPresente: number): PortaModel[] => {
+  return Array.from({ length: qtde }, (_, i) => {
+    const num = i + 1;
+    const temPresente = num === comPresente;
+    return new PortaModel(num, temPresente);
+  });
 };
-export const atualizarPortas = (portas:PortaModel[],modificacao:PortaModel):PortaModel[] => {
-  return portas.map((portaAntiga)=>{
-    const igualModificada = portaAntiga.numero === modificacao.numero
-    if (!igualModificada){
-      return modificacao.aberta?portaAntiga:portaAntiga.desselecionar()
+export const atualizarPortas = (
+  portas: PortaModel[],
+  modificacao: PortaModel
+): PortaModel[] => {
+  return portas.map((portaAntiga) => {
+    const igualModificada = portaAntiga.numero === modificacao.numero;
+    if (!igualModificada) {
+      return modificacao.aberta ? portaAntiga : portaAntiga.desselecionar();
     }
-    return modificacao
-  })
-
-
-
-}
+    return modificacao;
+  });
+};
