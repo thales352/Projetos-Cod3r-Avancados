77958423c8ab691b97f05d98a9ebb9ace4c381a4
Your Name <thalesiftm@gmail.com>
2022-02-20 16:59:26 -0300

    main

--- site-coracao-de-madeira/components/Layout.jsx
+++ site-coracao-de-madeira/components/Layout.jsx
@@ -1,34 +1,3 @@
-import Head from 'next/head';
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Container,
-  ThemeProvider,
-} from '@mui/material';
-import { useThemes } from '../utils/Theme';
-
 export default function Layout({ children }) {
-  const theme = useThemes();
-  return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>NeoSign</title>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography>NeoSign Comunicação Visual</Typography>
-        </Toolbar>
-      </AppBar>
-      <Container>{children}</Container>
-      <footer>
-        <Typography>Todos os Direitos Reservados por Thales Miguel</Typography>
-      </footer>
-    </ThemeProvider>
-  );
+  return <>{children}</>;
 }
--- site-coracao-de-madeira/src/pages/_app.js
+++ site-coracao-de-madeira/src/pages/_app.js
@@ -1,11 +1,20 @@
-import '../styles/globals.css';
-import Layout from '../../components/Layout';
+import "../styles/globals.css";
+import Head from "next/head";
+import Layout from "../../components/Layout";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Coração de Madeira</title>
+        <link rel="icon" type="image/x-icon" href="/favicon.png"></link>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
